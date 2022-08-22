a6acacd65230748e7cac5371c9865214700cb1f1
Haziq Kamel <haziq@fireworks.my>
2022-08-22 17:46:39 +0800

    Completed Project

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,38 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by USER on 8/22/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: .blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: .green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: .red)
+        }
+    }
+    
+    func getBMIValue() -> String {
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .yellow
+    }
+}
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by USER on 8/22/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
